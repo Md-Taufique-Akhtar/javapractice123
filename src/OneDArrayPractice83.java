5d0d970ed4ed16fe33de5f4b6d3d7baf63aa59a2
HP <mdtaufiqueakhtar4@gmail.com>
2022-12-09 22:05:57 +0530

    pattern3

---
+++ src/OneDArrayPractice83.java
@@ -0,0 +1,2 @@
+public class OneDArrayPractice83 {
+}
---
+++ src/oneDArray82.java
@@ -0,0 +1,8 @@
+public class oneDArray82 {
+    public static void main(String args[]){
+        int []arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
